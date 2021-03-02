278a1edee271df167a91a773b24bcd8114c6dc6d
tmazyrko <tmazyrko@gmail.com>
2021-03-02 00:58:14 -0500

    ReadCSV.js test logs output to console

--- tests/ReadCSV.test.js
+++ tests/ReadCSV.test.js
@@ -1,8 +1,9 @@
 const CSVRead = require('../src/CSV/ReadCSV');
 const City = require('../src/Models/City')
 const expect = require('expect');
-test('Papaparse', async () => {
+test('Can read CSV file with Papa Parse', async () => {
     let filename  = 'data/worldcities.csv';
     let outData = await CSVRead.getRecords(filename, City);
     expect(outData.length).toBe(6)
+    console.log(outData);
 });
